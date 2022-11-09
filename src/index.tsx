6f25c475edc3428eea3f8e6e1e92a70fd0aac20a
Sultan <sembinovsultan@gmail.com>
2022-11-09 19:20:49 +0600

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { Provider } from 'react-redux'
+
+import store from './app/store'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NewsList from './pages/NewsList';
+import NewsPage from './pages/NewsPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+      <div className="p-5 m-auto">
+          <Switch>
+            <Route path="/news/:newsId">
+              <NewsPage />
+            </Route>
+            <Route path="/">
+              <NewsList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
